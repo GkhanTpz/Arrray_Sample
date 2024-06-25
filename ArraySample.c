4e5e2661b3d1aee0e75a12008a99111033e8205e
Gökhan TOPUZ <gokhan061@msn.com>
2024-06-25 22:38:34 +0300

    Update ArraySample.c

--- ArraySample.c
+++ ArraySample.c
@@ -2,40 +2,40 @@
 #include <stdlib.h>
 #define MAX 50
 
-void sentenceReading(char* sentence);
-int arrayLenght(char* sentence);
+void SentenceReading(char* sentence);
+int ArrayLenght(char* sentence);
 
 int main()
 {
     int lenght;
-    char* sentence = (char*)malloc(MAX * sizeof(char));
+    char *sentence = (char*)malloc(MAX * sizeof(char));
     printf("%s","Please enter a sentence: ");
-    sentenceReading(sentence);
-    lenght = arrayLenght(sentence);
+    SentenceReading(sentence);
+    lenght = ArrayLenght(sentence);
     printf("\nLenght of Array: %d",lenght);
     free(sentence);
 
     return 0;
 }
 
-void sentenceReading(char *sentence)
+void SentenceReading(char *sentence)
 {
     int i = 0;
     scanf("%c",&sentence[i]);
     while((sentence[i] != '\n') && i <= MAX)
     {
-            i++;
-            scanf("%c",&sentence[i]);
+        i++;
+        scanf("%c",&sentence[i]);
     }
     sentence[i] = '\0';
 
-     for(i = 0; sentence[i] != '\0'; i++)
+    for(i = 0; sentence[i] != '\0'; i++)
     {
         printf("%c",sentence[i]);
     }
 }
 
-int arrayLenght(char* sentence)
+int ArrayLenght(char* sentence)
 {
     int i;
     for(i = 0; sentence[i] != '\0'; i++);
